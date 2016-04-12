a5f1888ed723c7b69e5a5171ee72f6219255a96f
Jacob Dorpinghaus <jdorpinghaus@keystone-technologies.com>
2016-04-11 22:24:26 -0500

    Finished Caesar and added to batch file

--- caesar.py
+++ caesar.py
@@ -2,7 +2,7 @@ from secret import *
 import requests
 from splinter import Browser
 
-caeser_url='https://hdfw-tehgame.herokuapp.com/challenge/caesar'
+caesar_url='https://hdfw-tehgame.herokuapp.com/challenge/caesar'
 caesar_cracker='http://www.mygeocachingprofile.com/codebreaker.caesarcipher.aspx'
 get_headers = {'authorization': authorization}
 post_headers = {'authorization': authorization,'content-type':'application/json'}
@@ -17,10 +17,24 @@ def getCaesar():
 
 def getCaesarArray(caesar):
 		answers=[]
+		answers.append(caesar);
 		with Browser('chrome') as b:
 				b.visit(caesar_cracker)
 				b.find_by_css('#EncryptedText').fill(caesar)
 				b.find_by_css('#Key').fill('hackdfw')
 				b.find_by_css('#ButtonCodebreak').first.click()
 				result=b.find_by_css('#Message').text
-		for x in range(0,26)
+				splitted=result.split()
+		for x in range(0,26):
+				answers.append(splitted[(8*x)-1])
+		return answers
+		
+def postCaesar(array):
+		for x in array:
+				res=requests.post(caesar_url+"/verify",headers=post_headers,data='{"answer":"'+x+'"}')
+				if("Correct" in res.json()["message"]):
+						print("Success! "+x)
+						exit()
+caesar = getCaesar()
+solutions=getCaesarArray(caesar)
+postCaesar(solutions)
--- run.bat
+++ run.bat
@@ -3,5 +3,8 @@
 :LOOP
 ECHO Waiting for 5 minutes...
   PING -n 300 127.0.0.1>nul
+	ECHO Vigenere:
   python vigenere.py
+	ECHO Caesar:
+	python caesar.py
 GOTO LOOP
